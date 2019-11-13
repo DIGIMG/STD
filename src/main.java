8215d9b186656b0fb92ae7bb398ab1806a33663f
DIGIMG <Support@DIGIMG.net>
2019-11-13 15:33:45 +0330

    write 85% of project

--- src/main.java
+++ src/main.java
@@ -1,10 +1,41 @@
-
+import java.util.Scanner;
 public class main {
+    public static final int t_dars = 3;
+    public static final int t_std = 3;
 
-String name;
-String family;
-double number;
-int unit;
+
+
+    public static void main(String[] args) {
+        String name;
+        double[] number= new double[t_std];
+        Scanner input = new Scanner(System.in);
+        studednt[] std= new studednt[t_std];
+
+        for(int q=0;q<t_std;q++) /* دریافت نام دانش آموز */
+        {
+            System.out.println("Please Enter name "+q+1);
+            name = input.next();
+            std[q].setname(name);
+
+            for(int w=0;w<t_dars;w++) /* دریافت نمرات دروس */
+            {
+                System.out.println("Please Enter Number"+ w+1);
+                number[w] = input.nextDouble();
+                std[q].setnumber(number[w]);
+            }
+
+        }
+
+   for(int r=0;r<t_std;r++)
+   {
+       System.out.println("Name = " + std[r].getname() );
+       System.out.println("Avverage ="+ std[r].getavv() );
+   }
+
+
+
+    }
+
 
 
 }
--- src/studednt.java
+++ src/studednt.java
@@ -1,4 +1,36 @@
 public class studednt {
+public static final int t_dars = 3;
+public static final int t_std = 3;
+String tempname;
+double[] tempnumber = new double[3];
+int setindex=0;
+double sum =0 ;
+double avverage = 0 ;
+
+
+public void setname(String name)
+{
+    tempname = name ;
+}
+public String getname()
+{
+    return tempname;
+}
+public void setnumber(double number)
+{
+    tempnumber[setindex] = number;
+    setindex++;
+}
+
+public double getavv()
+{
+    for (int e =0 ; e < t_dars;e++)
+    {
+        sum =+ tempnumber[e];
+    }
+    avverage = sum / t_dars;
+    return avverage;
+}
 
 
 
